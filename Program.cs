ca7513c2be3f738b5fba5623be2fa3fee2775343
Olga <olga-rovnova@yandex.ru>
2022-06-13 00:46:37 +0300

    home work task8

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿//Задача 8: Напишите программу, которая на вход принимает число (N), 
+//а на выходе показывает все чётные числа от 1 до N.
+
+Console.Write("введите целое число N: ");
+string input = Console.ReadLine();
+int number = Convert.ToInt32(input);
+
+int count = number;
+
+if (number > 0)
+{
+    while (count > 1)
+    {
+        int div = number % 2;
+        if (div == 0)
+        {
+            Console.Write(number);
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+        number = number - 1;
+        count = count - 1;
+    }
+}
+if (number <= 0)
+{
+    while (count < 1)
+    {
+        int div = number % 2;
+        if (div == 0)
+        {
+            Console.Write(number);
+        }
+        else
+        {
+            Console.Write(" ");
+        }
+        number = number + 1;
+        count = count + 1;
+    }
+}
